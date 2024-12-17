71e7d7f7da39d5854fc35f75e76f5f1c7c7327d4
Mirzavaliyev <shavkatziyo98@gmail.com>
2024-12-17 21:56:07 +0500

    Initial commit

---
+++ talaba.py
@@ -0,0 +1,29 @@
+# class Talaba:
+#     """Talaba haqidagi malumot"""
+#     def __init__(self,ism,familiya,t_yil):
+#         self.ism = ism
+#         self.familiya = familiya
+#         self.t_yil = t_yil
+#     def tanishtir(self):
+#         print(f"Salom men {self.ism} {self.familiya} {self.t_yil} - yilda tug'ilganman")
+#     def ismayt(self):
+#         print(self.ism)
+#         return self.ism
+#
+# talaba4 = Talaba("Hasan","Akbarov",2004)
+# talaba4.tanishtir()
+# talaba4.ismayt()
+# class User:
+#     def __init__(self,name,surname,email,phone):
+#         self.name = name
+#         self.surname = surname
+#         self.email = email
+#         self.phone = phone
+#     def all_info(self):
+#         print(f"Name: {self.name} \nSurname: {self.surname}")
+#         print(f"Email: {self.email} \nPhone: {self.phone}")
+#     def name_uz(self):
+#         return self.name
+# user1 = User('Shavkatjon','Mirzavaliyev','shavkatziyo@mail.com',934954500)
+# user1.all_info()
+# user1.name_uz()
