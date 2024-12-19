73b83f4f5531e8cdf972544f604c7cfd1443ff03
Mirzavaliyev <shavkatziyo98@gmail.com>
2024-12-20 01:40:22 +0500

    working

--- oop_practice.py
+++ oop_practice.py
@@ -13,8 +13,31 @@
 # talaba1.fan_qosh('Matematika')
 # talaba1.fan_qosh("Fizika")
 # print(talaba1.info())
-
-
+class Fan:
+    def __init__(self,nomi,soatlar_soni):
+        self.nomi = nomi
+        self.soatlar_soni = soatlar_soni
+    def info(self):
+        return (f"Fanning nomi: {self.nomi}"
+                f"\nFanning Soatlar soni: {self.soatlar_soni}")
+fan1 = Fan('Fizika', 30)
+fan2 = Fan('Tarix', 45)
+fan3 = Fan('Ingliz tili', 80)
+print(fan1.info())
+print(fan2.info(), fan3.info())
+class Talaba:
+    def __init__(self,ismi,familyasi,fani):
+        self.ismi = ismi
+        self.familyasi = familyasi
+        self.fani = fani
+    def info(self):
+        return (f"Talaba ismi: {self.ismi}"
+                f"\nTalanba familyasi: {self.familyasi}"
+                f"\nO'qiydigan fani: {self.fani.nomi}"
+                f"\nFanning soatlari: {self.fani.soatlar_soni}")
+fan1 = Fan('Fizika',300)
+talaba1 = Talaba('Shaxnoz','Valiyeva', fan1)
+print(talaba1.info())
 
 
 
