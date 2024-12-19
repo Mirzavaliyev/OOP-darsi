3f1dc75eb066275b97a6497dc07db077d3ee2bff
Mirzavaliyev <shavkatziyo98@gmail.com>
2024-12-20 01:17:56 +0500

    work

---
+++ oop_practice.py
@@ -0,0 +1,23 @@
+# class Talaba:
+#     def __init__(self,ismi,yoshi):
+#         self.ismi = ismi
+#         self.yoshi = yoshi
+#         self.fanlar = []
+#     def fan_qosh(self,fan):
+#         self.fanlar.append(fan)
+#     def info(self):
+#         return (f"Talaba ismi {self.ismi}"
+#                 f"Talaba yoshi {self.yoshi}"
+#                 f" O'qiydigan fanlari {', '.join(self.fanlar) if self.fanlar else 'yoq'}")
+# talaba1 = Talaba('Shavkatjon Mirzavaliyev ', 26)
+# talaba1.fan_qosh('Matematika')
+# talaba1.fan_qosh("Fizika")
+# print(talaba1.info())
+
+
+
+
+
+
+
+
